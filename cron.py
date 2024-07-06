d1be15c67ad42a94ba1496e7db2e9e45e58276d5
Lachie <lachie.duncan12@gmail.com>
2024-07-06 12:11:11 +1000

    modified cron for refreshing data

--- cron.py
+++ cron.py
@@ -1,3 +1,5 @@
+import traceback
+
 import asyncio
 
 from sqlalchemy import delete, and_
@@ -12,27 +14,49 @@ from universe import Universe
 from models import Stock
 from database import SessionLocal
 
+def commit_changes(upsert_stmts, delete_stmt):
+    # initialise db connection
+    db = SessionLocal()
+    attempts = 0
+    while attempts < 3:
+        try:
+            attempts += 1
+            # execute upsert statement
+            db.execute(upsert_stmts)
+            # execute delete statement
+            db.execute(delete_stmt)
+            # commit changes
+            db.commit()
+            # close connection
+            db.close()
+            print("Changes commited")
+            return
+        except:
+            db.rollback()
+            print("Rolled back")
+
+    print("Error executing statements after three attempts")
+
 async def refresh_stock_data_by_exchange(exchange: str) -> None:
     if not (exchange == "ASX" or exchange == "NASDAQ"):
         raise Exception("Exchange must be ASX or NASDAQ")
 
     try:
         print("Updating data for exchange: ", exchange)
-        # initialise db connection
-        db = SessionLocal()
         # fetch all symbols for exchange
         stocks = await ApiClient().get_all_stocks_by_exchange(exchange)
         # keep track of updated symbols
         updated = []
         errored = []
-        max_calls = 50
+        to_upsert = []
+        max_calls = 100
         delay_per_call = 60 / max_calls
         # initialise a min_wait task
         min_wait = asyncio.create_task(asyncio.sleep(0))
         for quote in stocks:
             try:
                 min_cap = 50_000_000_000 if exchange == "NASDAQ" else 5_000_000_000
-                if quote['marketCap'] and quote['marketCap'] < min_cap:
+                if quote['marketCap'] is None or quote['marketCap'] < min_cap:
                     continue
 
                 # wait for min_wait
@@ -42,48 +66,47 @@ async def refresh_stock_data_by_exchange(exchange: str) -> None:
                 # create a new min_wait task
                 min_wait = asyncio.create_task(asyncio.sleep(delay_per_call))
                 if data:
-                    # upsert data
-                    stmt = insert(Stock).values(data)
-                    on_conflict_stmt = stmt.on_conflict_do_update(
-                        index_elements=['symbol'],
-                        set_=data
-                    )
-                    db.execute(on_conflict_stmt)
+                    # append data to upsert array
+                    to_upsert.append(data)
                     # append symbol to updated array
                     updated.append(quote['symbol'])
-                    print(f"Data updated for {quote['symbol']}", end="\r")
+                    print(f"Retreived data for {quote['symbol']}", end="\r")
 
             except Exception as e:
                 errored.append(quote['symbol'])
                 print(f"Could not refresh data for {quote['symbol']}: {str(e)}")
 
-        # delete all sybmols that were not updated
+        # upsert statement for updated symbols
+        insert_stmts = insert(Stock).values(to_upsert)
+        upsert_stmts = insert_stmts.on_conflict_do_update(
+            index_elements=['symbol'],
+            set_={c.key: c for c in insert_stmts.excluded}
+        )
+
+        # delete all symbols that were not updated
         delete_stmt = delete(Stock).where(
             and_(
                 ~Stock.symbol.in_(updated),
                 Stock.exchange == exchange
             )
         )
-        db.execute(delete_stmt)
-        # commit changes
-        db.commit()
+        
+        commit_changes(upsert_stmts, delete_stmt)
         # revalidate universe
         Universe().revalidate()
         print("Data updated for symbols", ",".join(updated))
         print("Update errored for symbols", ",".join(errored))
     except Exception as e:
+        traceback.print_exc()
         print(f"Error refreshing data for exchange {exchange}: {str(e)}")
-    finally:
-        # close db
-        db.close()
 
 def schedule_jobs(scheduler: AsyncIOScheduler) -> None:
     scheduler.add_job(
         refresh_stock_data_by_exchange,
         args=["ASX"],
-        trigger=CronTrigger(hour=15, minute=0, day_of_week='mon-fri'),
+        trigger=CronTrigger(hour=18, minute=0, day_of_week='mon-fri'),
         id="refresh_asx",
-        name="Refresh ASX data at 5pm AEST",
+        name="Refresh ASX data at 6pm AEST",
         max_instances=1
     )
 
@@ -92,6 +115,6 @@ def schedule_jobs(scheduler: AsyncIOScheduler) -> None:
         args=["NASDAQ"],
         trigger=CronTrigger(hour=8, minute=0, day_of_week='tue-sat'),
         id="refresh_nasdaq",
-        name="Refresh NASDAQ data at 9am AEST",
+        name="Refresh NASDAQ data at 8am AEST",
         max_instances=1
     )
